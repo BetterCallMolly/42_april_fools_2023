8cb2e30bb5fb3b1d60c3a21f6ca8ee5d32299a5a
Jonas ALLERHAND <jallerha@student.42.fr>
2023-04-01 10:13:39 +0200

    ex05: palindrome in perl

---
+++ ex05/palindrome.pl
@@ -0,0 +1,12 @@
+# Je n'ai jamais ecrit de code en Perl, mais je ne le ferais plus jamais.
+
+$input = <STDIN>;
+
+# Chomp retire la fin de ligne
+chomp($input);
+
+if ($input eq reverse $input) {
+    print("The string is a palindrome!\n");
+} else {
+    print("The string is not a palindrome.");
+}
